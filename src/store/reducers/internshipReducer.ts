a20e5e881b97f080a4cc2aa62d056d32c5b02c5f
orianelouzoun <oriane.louzoun@mail.huji.ac.il>
2021-05-21 05:39:09 +0300

    commit

--- src/store/reducers/internshipReducer.ts
+++ src/store/reducers/internshipReducer.ts
@@ -1,4 +1,4 @@
-import { InternshipAction, InternshipActionTypes, InternshipState } from "../../types/internship";
+import { InternshipAction, InternshipActionTypes, InternshipState,CreateInternshipAction } from "../../types/internship";
 
 const initialState: InternshipState = {
     internships: [],
@@ -13,6 +13,19 @@ export const internshipReducer = (state = initialState, action: InternshipAction
         case InternshipActionTypes.FETCH_INTERNSHIP_SUCCESS:
             return {loading: false, error: null, internships: action.payload}
         case InternshipActionTypes.FETCH_INTERNSHIP_ERROR:
+            return {loading: false, error: action.payload, internships: []}
+        default:
+            return state
+    }
+}
+
+export const CreateinternshipReducer = (state = initialState, action: CreateInternshipAction): InternshipState => {
+    switch (action.type) {
+        case InternshipActionTypes.CREATE_INTERNSHIP:
+            return {loading: true, error: null, internships: []}
+        case InternshipActionTypes.CREATE_INTERNSHIP_SUCCESS:
+            return {loading: false, error: null, internships: action.payload}
+        case InternshipActionTypes.CREATE_INTERNSHIP_ERROR:
             return {loading: false, error: action.payload, internships: []}
         default:
             return state
