8ea48a2ff916478f5f8870427b677b2665d64ab5
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-05-21 05:14:51 +0300

    DF

--- src/Components/LoginDialog.tsx
+++ src/Components/LoginDialog.tsx
@@ -37,13 +37,15 @@ export const LoginDialog = (props: LoginDialogProps) => {
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
 
-    const { access_token } = useTypedSelector(state => state.login)
+    const { access_token, user_id } = useTypedSelector(state => state.login)
     const { LoginUser } = useActions()
 
     const handleLogin = () => {
         LoginUser(email, password);
-        if(access_token) {
+
+        if (access_token) {
             localStorage.setItem('access_token', access_token);
+            localStorage.setItem('user_id', user_id);
             onDialogClose();
         }
     };
--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,12 +1,13 @@
 import {combineReducers} from "redux";
 import { internshipReducer } from "./internshipReducer";
-import {loginReducer, userReducer} from "./userReducer";
+import {getUserReducer, loginReducer, userReducer} from "./userReducer";
 
 
 export const rootReducer = combineReducers({
     user: userReducer,
     internships: internshipReducer,
-    login: loginReducer
+    login: loginReducer,
+    getUser: getUserReducer,
 })
 
 export type RootState = ReturnType<typeof rootReducer>
