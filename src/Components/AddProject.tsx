7c59a14b2f74686470fc2612ce2a48017c2b31d0
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-05-21 08:09:48 +0300

    asdas

--- src/Components/AddProject.tsx
+++ src/Components/AddProject.tsx
@@ -2,11 +2,7 @@ import React, { useReducer, useEffect, useState } from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
 import Card from '@material-ui/core/Card';
-<<<<<<< HEAD
 import { Avatar, Checkbox, Dialog } from '@material-ui/core';
-=======
-import { Avatar, Checkbox, duration } from '@material-ui/core';
->>>>>>> 9752c499fce8c5a41679adf6e0ef08e8c704466c
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -44,32 +40,6 @@ const useStyles = makeStyles((theme: Theme) =>
     })
 );
 
-<<<<<<< HEAD
-=======
-type State =
-    {
-        projectName: string
-        duration: number
-        difculty: number
-        aplydate: string
-        submitdate:string
-        topic:string
-        tags: string
-        description: string
-    };
-
-const initialState: State = {
-    projectName: 'demo',
-    duration: 0,
-    difculty: 0,
-    aplydate: '01/01/2000',
-    submitdate: '01/01/2000',
-    topic: '',
-    tags: '',
-    description: ''
-};
-
->>>>>>> 9752c499fce8c5a41679adf6e0ef08e8c704466c
 const AvailableThemes = ["hard", "medium", "easy"];
 
 export interface AddProjectProps {
@@ -79,7 +49,6 @@ export interface AddProjectProps {
 
 export const AddProject = (props: AddProjectProps) => {
     const classes = useStyles();
-<<<<<<< HEAD
     const { onClose, openDialog } = props;
     return (
         <Dialog open={openDialog} onClose={onClose}>
@@ -172,148 +141,6 @@ export const AddProject = (props: AddProjectProps) => {
                             href={'/'}
                         >
                             submit
-=======
-    const [projectName, setName] = useState<string>("")
-    const[duration, setDuration] = useState<number>(0)
-    const [aplydate, setAdate] = useState<string>("")
-    const [submitdate, setSdate] = useState<string>("")
-    const [dificulty, setDificulty] = useState<number>(0)
-    const [topic, setTopic] = useState<string>("")
-    const [tags, setTag] = useState<string>("")
-    const [description, setDescription] = useState<string>("")
-
-    const handleNameChange = (event: any) => {
-        setName(event.target.value)
-    };
-
-    const handleAdate = (event: any) => {
-        setAdate(event.target.value)
-    };
-
-    const handleSdate = (event: any) => {
-        setSdate(event.target.value)
-    };
-
-    const handleDurationChange = (event: any) => {
-        setDuration(event.target.value)
-    };
-
-    const handleDificultyChange = (event: any) => {
-        setDificulty(event.target.value)
-    };
-
-    const handleTopic = (event: any) =>
-    {
-        setTopic(event.target.value)
-    };
-
-    const handleTagChange = (event: any) => {
-        setTag(event.target.value)
-    };
-
-    const handleDescription = (event: any) =>
-    {
-        setDescription(event.target.value)
-    }
-
-    return (
-        <form className={classes.container} noValidate autoComplete="off">
-            <Card className={classes.card}>
-                <CardHeader className={classes.header} title="Add Project" />
-                <CardContent>
-                    <div>
-                        <TextField
-                            fullWidth
-                            id="name"
-                            type="name"
-                            label="Name"
-                            placeholder="Title"
-                            margin="normal"
-                            onChange={handleNameChange}
-                        />
-                        <TextField
-                            fullWidth
-                            id="aplication"
-                            type="date"
-                            label="last apllication"
-                            defaultValue="2017-05-24"
-                            margin="normal"
-                            onChange={handleAdate}
-                        />
-                        <TextField
-                            fullWidth
-                            id="date"
-                            type="date"
-                            label="due date"
-                            defaultValue="2017-05-24"
-                            margin="normal"
-                            onChange={handleSdate}
-                        />
-                        <TextField
-                            fullWidth
-                            id="duration"
-                            type="length"
-                            label="Duration"
-                            placeholder="Duration"
-                            margin="normal"
-                            onChange={handleDurationChange}
-                        />
-                        <TextField
-                            fullWidth
-                            id="description"
-                            type="description"
-                            label="description"
-                            placeholder=""
-                            margin="normal"
-                            onChange={handleDescription}
-                        />
-                        <TextField
-                            fullWidth
-                            id="topic"
-                            type="topic"
-                            label="topic"
-                            placeholder="machin learning"
-                            margin="normal"
-                            onChange={handleTopic}
-                        />
-                        <TextField
-                            fullWidth
-                            id="topics"
-                            type="topics"
-                            label="tags"
-                            placeholder="#python"
-                            margin="normal"
-                            onChange={handleTagChange}
-                        />
-                        <FormControl fullWidth component="fieldset" margin="normal">
-                            <FormLabel component="legend" focused={false}>
-                            </FormLabel>
-                            <FormGroup aria-label="Temas" row={true}>
-                                {AvailableThemes.map((theme, i) => (
-                                    <FormControlLabel
-                                        key={theme}
-                                        value={theme}
-                                        label={theme}
-                                        onChange={handleDificultyChange}
-                                        control={<Checkbox
-                                            value="checkedA"
-                                            inputProps={{ 'aria-label': 'Checkbox A' }}
-                                          />}
-                                    />
-                                ))}
-                            </FormGroup>
-                        </FormControl>
-                    </div>
-                </CardContent>
-                        <CardActions>
-                            <Button
-                                variant="contained"
-                                size="large"
-                                color="secondary"
-                                className={classes.submitBtn}
-                                disabled={false}>
-                                submit
->>>>>>> 9752c499fce8c5a41679adf6e0ef08e8c704466c
                     </Button>
                     </CardActions>
                 </Card>
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -7,12 +7,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import CardHeader from '@material-ui/core/CardHeader';
 import { Typography } from '@material-ui/core';
-<<<<<<< HEAD
 import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
-=======
-import axios from 'axios';
->>>>>>> 9752c499fce8c5a41679adf6e0ef08e8c704466c
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -39,7 +35,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 //state type
 
-<<<<<<< HEAD
     const { email, rank, user_name } = useTypedSelector(state => state.getUser)
     const { GetUser } = useActions()
 
@@ -49,36 +44,6 @@ const useStyles = makeStyles((theme: Theme) =>
             console.log(localStorage.getItem("user_id"));
         }
     }, [localStorage.getItem("user_id")]);
-=======
-type State =
-    {
-        name: string
-        email: string
-        points: number
-        image: string
-    };
-
-const initialState: State = {
-    name: 'test',
-    email: 'tset@com',
-    points: 0,
-    image: 'path'
-};
-
-
-export const Profile = async (user_id:string) => {
-    const [email, setEmail] = useState<string>("")
-    const [name, setName] = useState<string>("")
-    const[points, setPoints] = useState<number>(0)
-    const config = { headers: { 'Content-Type': 'multipart/form-data' } };
-
-    let fd = new FormData();
-    fd.append("user_id", user_id);
-    const response = await axios.post('http://127.0.0.1:4500/api/personalSpace/student',
-        fd, config);
-
-    const classes = useStyles();
->>>>>>> 9752c499fce8c5a41679adf6e0ef08e8c704466c
     return (
         <form className={classes.container} noValidate autoComplete="off">
             <Card className={classes.card}>
@@ -87,11 +52,7 @@ export const Profile = async (user_id:string) => {
                 <Avatar alt="Remy Sharp" src="\src\avatar.jpeg" />
                     <div>
                         <Typography variant="h6" component="h6">
-<<<<<<< HEAD
                             {user_name}
-=======
-                            response[]
->>>>>>> 9752c499fce8c5a41679adf6e0ef08e8c704466c
                         </Typography>
                         <Typography variant="h6" component="h6">
                             {email}
